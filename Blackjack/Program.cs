1b58e324a582459fe49564b7c62f4e139c93c5c5
mevitts <martevitts@gmail.com>
2024-02-28 00:31:08 -0600

    first variables and createCards(). Enumerated card ranks and created list of 312 cards. When drawing a card, will remove from deck and then have to rerun CreateCards()?

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿namespace Blackjack
+{
+    class Program
+    {
+        private int balance;
+        private const int TOTAL_CARDS = 312;
+        private const int MAX_HAND = 21;
+        private List<String> dealerHand;
+        private List<String> userHand;
+        private List<String> cards;
+
+
+        public List<String> CreateCards()
+        {
+
+            cards = new List<String>();
+            //24 loops because 6 decks of 4 suits
+            for (int i = 0; i < 24; i++) {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank))) {
+                    //excluding Ace1 and 2 because i have a combination which is Ace
+                    if (rank != Rank.Ace1 && rank != Rank.Ace2)
+                    {
+                        cards.Add(rank.ToString());
+                    }
+                }       
+            }
+
+            return cards;
+        }
+
+
+    }
+
+    enum Rank { Ace1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack = 10, Queen = 10, King = 10, Ace2 = 11, Ace = Ace1 | Ace2 }
+
+}
